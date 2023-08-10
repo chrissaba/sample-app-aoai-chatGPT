3e8b01b3422dbe8b7bdb38d74fe4c8fea7e7dc63
Chris Saba <chris.saba@neudesic.com>
2023-08-10 13:03:28 -0700

    test

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -71,40 +71,30 @@ const Chat = () => {
                 
                 const reader = response.body.getReader();
                 let runningText = "";
+                let allNewMessages: ChatMessage[] = [];
                 while (true) {
                     const {done, value} = await reader.read();
                     if (done) break;
-                    let newAnswers: string[];
-                    var text = new TextDecoder("utf-8").decode(value);
+                    let text = new TextDecoder("utf-8").decode(value);
                     const objects = text.split("\n");
                     objects.forEach((obj) => {
                         try {
                             runningText += obj;
                             result = JSON.parse(runningText);
-                            setShowLoadingMessage(false);
-                            setAnswers(prevAnswers => {
-                                const newAnswers = [
-                                  ...prevAnswers, 
-                                  userMessage,
-                                  ...result.choices[0].messages
-                                ];
-                              
-                                return newAnswers; 
-                              })
-                            console.log("Saved to localStorage:", newAnswers);
-
+                            allNewMessages.push(...result.choices[0].messages);
                             runningText = "";
                         }
                         catch { }
                     });
                 }
-                let newAnswers;
-
+                console.log("All new messages:", allNewMessages);   // This is the array of messages to add to the state        
+                // After the while loop, update the state once:
                 setAnswers(prevAnswers => {
-                  newAnswers = [...prevAnswers, userMessage, ...result.choices[0].messages];
-                
-                  return newAnswers;
-                });                
+                    const newAnswers = [...prevAnswers, userMessage, ...allNewMessages];
+                localStorage.setItem('chatMessages', JSON.stringify(newAnswers)); // Setting to localStorage directly here
+                return newAnswers;
+            });
+
 
             }
             
@@ -130,9 +120,7 @@ const Chat = () => {
             setShowLoadingMessage(false);
             abortFuncs.current = abortFuncs.current.filter(a => a !== abortController);
         }
-        useEffect(() => {
-            localStorage.setItem('chatMessages', JSON.stringify(answers));
-          }, [answers]);
+        
         return abortController.abort();
     };
 
